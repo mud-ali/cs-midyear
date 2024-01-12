8a14760c2637c5b4c6771a28c4108e070e3d0235
ibrahimwichka <wikipediaofsoccer@gmail.com>
2024-01-12 11:38:51 -0500

    added create table func and topic name intialization

--- api/index.py
+++ api/index.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import sqlite3
 import json
+
+from utils.executeQueries import create_tables, insert_topic_details
 
 app = Flask(__name__)
 
-@app.route("/quiztaken", methods=["GET","POST"])
-def hello_world():
-    return "ack"
+db = sqlite3.connect('../db/debate.db')
+create_tables(db)
+
+@app.route("/submit_topic", methods=["GET","POST"])
+def submit_topic():
+    if request.method == "POST":
+        topic_name = request.form['topic_name']
+    insert_topic_details(db, topic_name)
+
+
+@app.route("/store_opinion", methods = ["GET", "POST"])
+def store_opinion():
+    return
+
+@app.route("/match_debater", methods = ["GET", "POST"])
+def match_debater():
+    return
+
+@app.route("/create_debate",methods = ["GET", "POST"])
+def create_debate():
+    return
 
---
+++ api/utils/executeQueries.py
@@ -0,0 +1,48 @@
+import sqlite3
+import json
+
+def create_tables(db):
+    create_queries = [
+        """CREATE TABLE IF NOT EXISTS user(
+                user_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+                user_first_name VARCHAR,
+                user_last_name VARCHAR,
+                password VARCHAR,
+                points INTEGER,
+                level VARCHAR,
+                dob INTEGER,
+                region VARCHAR,
+                timezone VARCHAR
+            )
+        """, 
+        """CREATE TABLE IF NOT EXISTS topic(
+                topic_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+                topic_name VARCHAR, 
+                q1 TEXT
+                q1_options TEXT
+                q2 TEXT
+                q2_options TEXT
+                q3 TEXT
+                q3_options TEXT
+            )
+        """,
+        """CREATE TABLE IF NOT EXISTS opinions(
+                user_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+                topic_id INTEGER PRIMARY KEY NOT NULL,
+                opinion1 TEXT NOT NULL,
+                opinion2 TEXT NOT NULL,
+                opinion3 TEXT NOT NULL, 
+                FOREIGN KEY(topic_id) REFERENCES topic(topic_id),
+                FOREIGN KEY(user_id2) REFERENCES user(user_id)
+            )
+        """
+    ]
+
+    for create_query in create_queries:
+        db.execute(create_query)
+    db.commit()
+
+def insert_topic_details(db, topic_name):
+    db_cursor = db.cursor()
+    db_cursor.execute("INSERT INTO topic (topic_name) VALUES (?)", (topic_name))
+    db.commit()
