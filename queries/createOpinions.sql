bcc458cd35668d1e8152eef2895b1d98a5781a39
ibrahimwichka <wikipediaofsoccer@gmail.com>
2024-01-12 11:38:06 -0500

    deleted votes for now and edited opinions/topic

--- queries/createOpinions.sql
+++ queries/createOpinions.sql
@@ -1,7 +1,9 @@
 CREATE TABLE IF NOT EXISTS opinions(
     user_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
     topic_id INTEGER PRIMARY KEY NOT NULL,
-    opinion TEXT NOT NULL, 
+    opinion1 TEXT NOT NULL,
+    opinion2 TEXT NOT NULL,
+    opinion3 TEXT NOT NULL, 
     FOREIGN KEY(topic_id) REFERENCES topic(topic_id),
     FOREIGN KEY(user_id2) REFERENCES user(user_id)
 );
--- queries/createVotes.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS topic(
-    debate_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
-    debate_count INTEGER,
-    FOREIGN KEY(debate_count) REFERENCES debate_public(debate_count)
-);
