02b6dedab7188ba728825d44491e266e4f76f9d0
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-01-12 16:27:25 -0500

    add submit button

--- app/create_topic/page.jsx
+++ app/create_topic/page.jsx
@@ -25,6 +25,7 @@ export default function create_topic(props) {
                     <label htmlFor="a1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
                     <input type="text" id="a1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
                 </div>
+                
                 <div className="question">
                     <label htmlFor="q1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 2:</label>
                     <input type="text" id="q1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
@@ -37,7 +38,7 @@ export default function create_topic(props) {
                     <label htmlFor="a1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
                     <input type="text" id="a1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
                 </div>
-                {/* add submit button here */}
+                <button type="submit">Submit</button>
             </form>
         </main>
     );
