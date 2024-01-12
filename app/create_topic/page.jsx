db4a66655ade0f42677a62eb852608e6952765e9
ibrahimwichka <wikipediaofsoccer@gmail.com>
2024-01-12 16:08:40 -0500

    new commit

--- app/create_topic/page.jsx
+++ app/create_topic/page.jsx
@@ -26,16 +26,16 @@ export default function create_topic(props) {
                     <input type="text" id="a1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
                 </div>
                 <div className="question">
-                    <label htmlFor="q1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 2:</label>
-                    <input type="text" id="q1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
-                    <label htmlFor="a1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
-                    <input type="text" id="a1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                    <label htmlFor="q2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 2:</label>
+                    <input type="text" id="q2" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                    <label htmlFor="a2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
+                    <input type="text" id="a2" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
                 </div>
                 <div className="question">
-                    <label htmlFor="q1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 3:</label>
-                    <input type="text" id="q1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
-                    <label htmlFor="a1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
-                    <input type="text" id="a1" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                    <label htmlFor="q3" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 3:</label>
+                    <input type="text" id="q3" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                    <label htmlFor="a3" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
+                    <input type="text" id="a3" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
                 </div>
                 {/* add submit button here */}
             </form>
