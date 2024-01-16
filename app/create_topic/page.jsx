425715b8935b3443db27336c9fae373a75324a06
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-01-16 12:11:14 -0500

    make some css changes (broken)

--- app/create_topic/page.jsx
+++ app/create_topic/page.jsx
@@ -18,25 +18,26 @@ export default function create_topic(props) {
                     <label htmlFor="topic_desc" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter a description:</label>
                     <input type="text" id="topic_desc" placeholder="Please describe your topic!" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
                 </div>
-                <div className="question">
-                    <label htmlFor="q1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 1:</label>
-                    <input type="text" id="q1" placeholder="What's the question?" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
-                    <label htmlFor="a1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
-                    <input type="text" id="a1" placeholder="Enter the option choices!" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+                <div className="question flex flex-wrap justify-between">
+                    <label htmlFor="q1" className="w-2/5 mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 1:</label>
+                    <label htmlFor="a1" className="w-2/5 mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
+                    <textarea type="text" id="q1" className="resize-none h-48 block w-2/5 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What's the question?"/>
+                    <textarea type="text" id="a1" className="resize-none h-48 block w-2/5 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter the option choices"/>
+                </div>
+                <div className="question flex flex-wrap justify-between">
+                    <label htmlFor="q2" className="w-2/5 mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 1:</label>
+                    <label htmlFor="a2" className="w-2/5 mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
+                    <textarea type="text" id="q2" className="resize-none h-48 block w-2/5 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What's the question?"/>
+                    <textarea type="text" id="a2" className="resize-none h-48 block w-2/5 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter the option choices"/>
+                </div>
+                <div className="question flex flex-wrap justify-between">
+                    <label htmlFor="q3" className="w-2/5 mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 1:</label>
+                    <label htmlFor="a3" className="w-2/5 mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
+                    <textarea type="text" id="q3" className="resize-none h-48 block w-2/5 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What's the question?"/>
+                    <textarea type="text" id="a3" className="resize-none h-48 block w-2/5 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter the option choices"/>
                 </div>
                 
-                <div className="question">
-                    <label htmlFor="q2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 2:</label>
-                    <input type="text" id="q2" placeholder="What's the question?" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
-                    <label htmlFor="a2" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
-                    <input type="text" id="a2" placeholder="Enter the option choices!" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
-                </div>
-                <div className="question">
-                    <label htmlFor="q3" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Question 3:</label>
-                    <input type="text" id="q3" placeholder="What's the question?" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
-                    <label htmlFor="a3" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Option choices:</label>
-                    <input type="text" id="a3" placeholder="Enter the option choices!" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
-                </div>
+                
                 <button type="submit">Submit</button>
             </form>
         </main>
