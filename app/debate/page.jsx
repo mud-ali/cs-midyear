f329ccc2ff54e643322e6fc996889c10c154736b
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-01-17 08:32:14 -0500

    add css module

--- app/debate/page.jsx
+++ app/debate/page.jsx
@@ -3,14 +3,14 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Chatbox from "@/components/chatbox";
-// import styles from "@/styles/join.module.css";
+import styles from "@/styles/debateRoom.module.css";
 
 export default function join(props) {
   return (
     <main>
       <Navbar at="topic-create" />
       <div className="message-box">
-        <h2 className="{styles.debate-room-title}">Debate Room:</h2>
+        <h2 className={styles.debate-room-title}>Debate Room:</h2>
         <Chatbox />
       </div>
     </main>
