d050329c37c1244e32dbb119703436116777b304
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-01-18 10:25:45 -0500

    add login prop to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -165,7 +165,7 @@ export default function Navbar(props: navprops) {
             <li>
               <a
                 href="/login/"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent ${props.at.startsWith("login") ? "text-blue-700 dark:text-blue-500" : "dark:text-white"}`}
               >
                 Login
               </a>
