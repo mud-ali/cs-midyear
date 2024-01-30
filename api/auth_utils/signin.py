55cfa6df5c4d877b7dac55884e7c56806e8f0d25
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-01-30 11:15:08 -0500

    fix signin func

--- api/auth_utils/signin.py
+++ api/auth_utils/signin.py
@@ -1,11 +1,12 @@
 import hashlib
 import sqlite3
 
-def verify_user(db : sqlite3.Connection, username : str, password : str) -> bool:
+def verify_user(username : str, password : str) -> bool:
+    db = sqlite3.connect('db/debate.db')
     user = db.cursor().execute(
         """
         SELECT * FROM user WHERE username = ?
-        """, (username)).fetchone()
+        """, (username,)).fetchone()
     if user is None:
         return False
-    return user['password'] == hashlib.sha256(password).hexdigest()
+    return user['password'] == hashlib.sha256(password.encode()).hexdigest()
